faf9672cee6a4e80f4ae84aec5a94babe77984b9
jlhillery999 <jlhillery999@gmail.com>
2024-03-24 00:17:16 -0400

    removed provider from firebase web app

--- dev/main.tf
+++ dev/main.tf
@@ -15,7 +15,6 @@ resource "google_project_service" "firebase_api" {
 }
 
 resource "google_firebase_web_app" "this" {
-    provider = google-beta
     project = var.project_id
     display_name = var.firebase_web_app_name
 
