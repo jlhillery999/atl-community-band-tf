275967b491f721a7c8809ae2d6850ba2ccc9c54f
jlhillery999 <jlhillery999@gmail.com>
2024-03-24 00:10:17 -0400

    commented firebase

--- dev/main.tf
+++ dev/main.tf
@@ -5,19 +5,19 @@ resource "google_project_service" "cloud_resource_manager_api" {
   disable_dependent_services = true
 }
 
-resource "google_project_service" "firebase_api" {
-  project = var.project_id
-  service = "firebase.googleapis.com"
+# resource "google_project_service" "firebase_api" {
+#   project = var.project_id
+#   service = "firebase.googleapis.com"
 
-  disable_dependent_services = true
+#   disable_dependent_services = true
   
-  depends_on = [ google_project_service.cloud_resource_manager_api ]
-}
+#   depends_on = [ google_project_service.cloud_resource_manager_api ]
+# }
 
-resource "google_firebase_web_app" "this" {
-    provider = google-beta
-    project = var.project_id
-    display_name = var.firebase_web_app_name
+# resource "google_firebase_web_app" "this" {
+#     provider = google-beta
+#     project = var.project_id
+#     display_name = var.firebase_web_app_name
 
-    depends_on = [ google_project_service.firebase_api ]
-}
+#     depends_on = [ google_project_service.firebase_api ]
+# }
