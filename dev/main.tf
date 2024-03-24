847051ba90353560be1954d2810351e92352aebc
jlhillery999 <jlhillery999@gmail.com>
2024-03-24 00:07:19 -0400

    enabled cloud resource manager api

--- dev/main.tf
+++ dev/main.tf
@@ -1,12 +1,23 @@
+resource "google_project_service" "cloud_resource_manager_api" {
+  project = var.project_id
+  service = "cloudresourcemanager.googleapis.com"
+
+  disable_dependent_services = true
+}
+
 resource "google_project_service" "firebase_api" {
   project = var.project_id
   service = "firebase.googleapis.com"
 
   disable_dependent_services = true
+  
+  depends_on = [ google_project_service.cloud_resource_manager_api ]
 }
 
 resource "google_firebase_web_app" "this" {
     provider = google-beta
     project = var.project_id
     display_name = var.firebase_web_app_name
+
+    depends_on = [ google_project_service.firebase_api ]
 }
