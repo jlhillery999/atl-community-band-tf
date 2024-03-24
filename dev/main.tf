74bd8786c9a24d65548acb3deb46d635968f5f8e
jlhillery999 <jlhillery999@gmail.com>
2024-03-24 00:04:11 -0400

    enabled firebase api

--- dev/main.tf
+++ dev/main.tf
@@ -1,3 +1,10 @@
+resource "google_project_service" "firebase_api" {
+  project = var.project_id
+  service = "firebase.googleapis.com"
+
+  disable_dependent_services = true
+}
+
 resource "google_firebase_web_app" "this" {
     provider = google-beta
     project = var.project_id
