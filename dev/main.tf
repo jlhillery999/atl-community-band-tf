df17d20c3bbcd638553259c17ad1d1981414f0ec
jlhillery999 <jlhillery999@gmail.com>
2024-03-26 19:26:44 -0400

    removed kms key

--- dev/main.tf
+++ dev/main.tf
@@ -43,41 +43,34 @@ resource "google_firebase_web_app" "this" {
   depends_on = [ google_firebase_project.this ]
 }
 
-resource "google_kms_key_ring" "web_app" {
-  project  = var.project_id
-  name     = var.web_app_key_ring_name
-  location = var.project_region
+# resource "google_kms_key_ring" "web_app" {
+#   project  = var.project_id
+#   name     = var.web_app_key_ring_name
+#   location = var.project_region
 
-  depends_on = [ google_project_service.cloud_kms_api ]
-}
+#   depends_on = [ google_project_service.cloud_kms_api ]
+# }
 
-resource "google_kms_crypto_key" "firebase_api_key" {
-  name     = var.firebase_api_key_name
-  key_ring = google_kms_key_ring.web_app.id
-  purpose  = "ENCRYPT_DECRYPT"
-  rotation_period = "7776000s"
-  version_template {
-    algorithm         = "GOOGLE_SYMMETRIC_ENCRYPTION"
-    protection_level  = "SOFTWARE"
-  }
-}
+# resource "google_kms_crypto_key" "firebase_api_key" {
+#   name     = var.firebase_api_key_name
+#   key_ring = google_kms_key_ring.web_app.id
+#   purpose  = "ENCRYPT_DECRYPT"
+#   rotation_period = "7776000s"
+#   version_template {
+#     algorithm         = "GOOGLE_SYMMETRIC_ENCRYPTION"
+#     protection_level  = "SOFTWARE"
+#   }
+# }
 
 resource "google_secret_manager_secret" "firebase_api_key" {
   secret_id = var.firebase_api_key_secret_id
   project = var.project_id
 
   replication {
-    auto {
-      customer_managed_encryption {
-        kms_key_name = var.firebase_api_key_name
-      }
-    }
+    auto {}
   }
 
-  depends_on = [ 
-    google_project_service.secret_manager_api,
-    google_kms_crypto_key.firebase_api_key    
-  ]
+  depends_on = [ google_project_service.secret_manager_api ]
 }
 
 resource "google_secret_manager_secret_version" "firebase_api_key" {
